546721a7c0add160a0546b33a8b60c6df8108255
JorJimLop <jorjimlop@outlook.com>
2021-07-15 19:47:21 -0600

    Todo el proyecto

---
+++ admin/config/global.php
@@ -0,0 +1,23 @@
+<?php 
+//ip de la pc servidor base de datos
+define("DB_HOST", "185.156.219.31"); //localhost
+
+// nombre de la base de datos
+define("DB_NAME", "i146384_control");
+
+
+//nombre de usuario de base de datos
+define("DB_USERNAME", "i146384_adeinvi");
+//define("DB_USERNAME", "u222417_admin");
+
+//conraseña del usuario de base de datos
+define("DB_PASSWORD", "m6zym*lxn]9S");
+//define("DB_PASSWORD", "Enero2020Admin");
+
+//codificacion de caracteres
+define("DB_ENCODE", "utf8");
+
+//nombre del proyecto
+define("PRO_NOMBRE", "CompartiendoCodigos");
+ 
+ ?>
